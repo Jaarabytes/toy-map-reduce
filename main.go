b120535a2ca83ad2573cd3a54eaf81396e83efc7
jaarabytes <xavierandole78@gmail.com>
2024-10-18 12:47:12 +0300

    feat: add the map reduce func
    Hell yeah. Also, concurrency is great

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+func main () {
+  scanner := bufio.NewScanner(os.Stdin)
+  var input []string
+  for scanner.Scan() {
+    input = append(input, scanner.Text())
+    if len(input) == 0 {
+    }
+  }
+
+  mapped := make(chan map[string]int)
+  result := make([]string, 0)
+  var wg sync.WaitGroup
+
+  for _, line := range input {
+    wg.Add(1)
+    go func (l string) {
+      defer wg.Done()
+      mapped <- mapFunc(l)
+    }(line)
+  }
+
+  // Who doesn't love concurrency
+  go func () {
+     wg.Wait()
+     close(mapped)
+  }()
+
+  reduced := reduceFunc(mapped)
+
+  for word, count := range reduced {
+    wordCountString := "{" + word + " " + strconv.Itoa(count) + "}"
+    result = append(result, wordCountString)
+  }
+  fmt.Println(result)
+}
+
+func mapFunc (input string) map[string]int {
+  result := make(map[string]int)
+  words := strings.Fields(input)
+  for _, word := range words {
+    result[strings.ToLower(word)]++
+  }
+  return result
+}
+
+func reduceFunc (mapped <- chan map[string]int) map[string]int {
+    result := make(map[string]int)
+    for m := range mapped {
+    for word, count := range m {
+        result[word] += count
+    }
+  }
+  return result
+}
